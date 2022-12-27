99670876c5432ba956c05d048908f5f7ed998420
vKuptsova <kuptsowa.vika@gmail.com>
2022-12-27 20:32:19 +0300

    feat: add getAverage function

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -51,8 +51,11 @@ function getCircleCircumference(radius) {
  *  10, 0  => 5
  *  -3, 3  => 0
  */
-function getAverage(/* value1, value2 */) {
-  throw new Error('Not implemented');
+function getAverage(value1, value2) {
+  if (((value1 + value2) / 2) === Infinity) {
+    return value1;
+  }
+  return ((value1 + value2) / 2);
 }
 
 /**
